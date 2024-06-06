cb14b3a346f1ff8a0062d286ec3a09d4ec3960be
Kevin Bonds <kevin.bonds@gmail.com>
2024-06-05 20:01:38 -0500

    Added Sorting of the project from Asc to Desc vise versa

--- resources/js/Components/SelectInput.jsx
+++ resources/js/Components/SelectInput.jsx
@@ -1,16 +1,16 @@
-import { children, forwardRef, useRef } from 'react';
+import { children, forwardRef, useRef } from "react";
 
-export default forwardRef(function SelectInput({className = '', children, ...props }, ref) {
+export default forwardRef(function SelectInput(
+    { className = "", children, ...props },
+    ref
+) {
     const input = ref ? ref : useRef();
-
-
 
     return (
         <select
             {...props}
-
             className={
-                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
+                "border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm " +
                 className
             }
             ref={input}
